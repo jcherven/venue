3b33bb7ea8716895b0d34f998dba96a946e57e92
jcherven <cherven.james@gmail.com>
2020-03-08 23:44:10 -0400

    Adds artist logo, Adjusts position in header

--- src/components/featured/index.jsx
+++ src/components/featured/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Carousel from "./Carousel";
 
+import roselia_logo from "../../resources/images/D4l379Roselia-Logo-6XL7FN.png";
+
 const Featured = () => {
   return (
     <div style={{ position: "relative" }}>
       <Carousel />
-      <div className="artist_name">
-        <div className="wrapper">Roselia</div>
+      <div
+        className="artist_name"
+        style={{ border: "none", position: "relative", top: "-130px" }}
+      >
+        <div className="wrapper" style={{ border: "none" }}>
+          <img
+            src={roselia_logo}
+            alt="Roselia Logo"
+            style={{ position: "relative", left: "-135px", top: "-70px" }}
+          />
+        </div>
       </div>
     </div>
   );
