6e146d431fcce8987424534ad0149f8a1f3e9ef3
jcherven <jamiecherven@gmail.com>
2020-12-17 13:35:32 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import './resources/styles.css';
+import Header from './components/header_footer/Header.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">yubi yubi</div>
+      <div>
+        <div className="App">yubi yubi</div>
+        <Header />
+      </div>
     );
   }
 }
